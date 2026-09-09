9743b99bbc1be8f4d209f40cb46a0b0d662f3d57
shrey200634 <dshrey60@gmail.com>
2026-09-10 03:01:01 +0530

    fix the open api config to route the api gateway no the user service and the notification service

--- notification-service/src/main/java/com/assignment/notification_service/config/OpenApiConfig.java
+++ notification-service/src/main/java/com/assignment/notification_service/config/OpenApiConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +19,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .info(new Info()
                         .title("Notification Service API")
                         .version("1.0.0")
